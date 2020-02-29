81767c7838a569ce35d6b9bca9fa4c43a08b919d
rzlihang <zhou.liha@husky.neu.edu>
2020-02-29 12:20:02 -0500

    small changes

--- script/test.js
+++ script/test.js
@@ -1,24 +1,25 @@
-// const log = (data) => {
-//     console.log(data)
-// };
-// const convert = (label) => 10;
-//
-// const testFunction = (n) => {
-//     const regEx = /(^|\+|-|\*|\/)([A-Z]+[1-9][0-9]*)/gi;
-//     let str = 'A1+B2+5+7';
-//     let matches = [];
-//     let matchRes = null;
-//     while ((matchRes = regEx.exec(str)) != null) {
-//         matches.push(matchRes[2]);
-//     }
-//     log(matches);
-//     for (let i = 0; i < matches.length; i++) {
-//         let substr = matches[i];
-//         str = str.replace(substr, ('convert("' + substr + '")'));
-//     }
-//
-//     log(eval(str));
-// };
-//
-// testFunction(2);
-//
+const log = (data) => {
+    console.log(data)
+};
+const convert = (label) => 10;
+
+const testFunction = (n) => {
+    const regEx = /(^|\+|-|\*|\/)(SUM\(([A-Z]+[1-9][0-9]*)\))/gi;
+    let str = 'A1+SUM(B2)+5+7';
+    let matches = [];
+    let matchRes = null;
+    while ((matchRes = regEx.exec(str)) != null) {
+        log(matchRes);
+        matches.push(matchRes[2]);
+    }
+    log(matches);
+    for (let i = 0; i < matches.length; i++) {
+        let substr = matches[i];
+        str = str.replace(substr, ('convert("' + substr + '")'));
+    }
+
+    log(str);
+};
+
+testFunction(2);
+
