f0f7a8f21f06b1e8592e9170ef04fd680aa4188e
Lihang Zhou <zhou.liha@husky.neu.edu>
2020-03-01 15:40:34 -0500

    added circular reference checking(2+)

--- script/test.js
+++ script/test.js
@@ -3,23 +3,29 @@ const log = (data) => {
 };
 const convert = (label) => 10;
 
-const testFunction = (n) => {
-    const regEx = /(^|\+|-|\*|\/)(SUM\(([A-Z]+[1-9][0-9]*)\))/gi;
-    let str = 'A1+SUM(B2)+5+7';
+const testFunction = () => {
+    const regEx0 = /(^|\+|-|\*|\/|\()([A-Z]+[1-9][0-9]*)/gi;
+    const regEx = /(^|\+|-|\*|\/)(SUM\(([A-Z0-9,:]*)\))/gi;
+    const regEx2 = /(,)*([A-Z]+[1-9][0-9]*)(^|:([A-Z]+[1-9][0-9]*))*/gi;
+    let str = 'A1+SUM(B2:B3,B9)+5+7';
     let matches = [];
     let matchRes = null;
     while ((matchRes = regEx.exec(str)) != null) {
-        log(matchRes);
-        matches.push(matchRes[2]);
+        //log(matchRes);
+        matches.push([matchRes[2], matchRes[3]]);
     }
-    log(matches);
+
+    while((matchRes=regEx2.exec(str))!=null){
+        log(matchRes);
+    }
+    //log(matches);
     for (let i = 0; i < matches.length; i++) {
         let substr = matches[i];
-        str = str.replace(substr, ('convert("' + substr + '")'));
+        str = str.replace(substr[0], ('convert(' + substr[1] + ')'));
     }
 
     log(str);
 };
 
-testFunction(2);
+testFunction();
 
