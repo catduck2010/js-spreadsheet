5634042bdf5a6e3843e40ef22aa986d24733a0cd
rzlihang <zhou.liha@husky.neu.edu>
2020-03-03 02:00:38 -0500

    try to add SUM() function

--- script/test.js
+++ script/test.js
@@ -21,26 +21,31 @@ const sumBlock = (str) => {
 const testFunction = () => {
     const regEx0 = /(^|\+|-|\*|\/|\()([A-Z]+[1-9][0-9]*)/gi;
     const sumEx = /(^|\+|-|\*|\/)(SUM\(([A-Z0-9,:]*)\))/gi;
+    const regEx = /(^|\+|-|\*|\/|\(|:|,)(SUM\(|[A-Z]+[1-9][0-9]*)/gi;
     const regEx2 = /(,)*([A-Z]+[1-9][0-9]*)(^|:([A-Z]+[1-9][0-9]*))*/gi;
     let str = 'A1+SUM(B2:B3,B9)+5+7';
     let matches = [];
     let matchRes = null;
-    while ((matchRes = sumEx.exec(str)) != null) {
-        //log(matchRes);
-        matches.push([matchRes[2], matchRes[3]]);
-    }
-    log(matches);
-
-    while ((matchRes = regEx2.exec(str)) != null) {
+    while ((matchRes = regEx.exec(str)) != null) {
         log(matchRes);
+        //matches.push([matchRes[2], matchRes[3]]);
     }
-    //log(matches);
-    for (let i = 0; i < matches.length; i++) {
-        let substr = matches[i];
-        str = str.replace(substr[0], ('convert(' + substr[1] + ')'));
-    }
+    // log(matches);
+    //
+    // while ((matchRes = regEx2.exec(str)) != null) {
+    //     log(matchRes);
+    // }
+    // //log(matches);
+    // for (let i = 0; i < matches.length; i++) {
+    //     let substr = matches[i];
+    //     str = str.replace(substr[0], ('convert(' + substr[1] + ')'));
+    // }
+    //
+    // log(str);
+};
 
-    log(str);
+const testFunction2 = (str1, str2) => {
+
 };
 
 testFunction();
